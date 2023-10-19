451825f74072d0173ac7daba0c2a59cba76049c4
Tanuj <Tanuj@cmu.edu>
2023-10-18 19:41:18 -0700

    update simulate.py + patch up a meaty docstring

--- Meaty-docstrings/Validation_docstrings.py
+++ Meaty-docstrings/Validation_docstrings.py
@@ -1,12 +1,9 @@
 from pynput.keyboard import Key, KeyCode
 from json import JSONEncoder, JSONDecoder
-from typing import Union, Optional, TypedDict
+from typing import Union, TypedDict
 from pynput.keyboard import Key, KeyCode
 
-class Keystroke:
-    """
-    A class used to represent a keystroke. The validity is held in Keystroke.valid
-    """
+
 def filter_non_typable_chars(input_string: str) -> str:
     """
     Filter out non-typable characters from a string.
@@ -15,21 +12,10 @@ def is_key_valid(key: Union[Key, KeyCode, str]) -> bool:
     """
     Function to check if the key is valid.
     """
-def __init__(self, key: str, time: Optional[float]):
-        # Implement LegalKey here? Or can Keystrokes have illegal keys?
-        if not isinstance(key, str):
-            raise TypeError('key must be a string')
-        if not isinstance(time, float) and time is not None:
-            raise TypeError('time must be a float or None')
-        self.key = key
-        self.time = time
-        self.valid = is_key_valid(key)
-
 class Keystroke:
     """
     A class used to represent a keystroke. The validity is held in Keystroke.valid
-    """
-    
+    """    
 class Log(TypedDict):
     """
     A class used to represent a log. The logfile is a list of logs.
--- simulate.py
+++ simulate.py
@@ -1,8 +1,8 @@
 from keyLogger import KeyLogger
 from config import ABSOLUTE_SIM_FILEPATH
+from validation import filter_non_typable_chars
 from keySimulator import KeySimulator
 from time import sleep
-import string
 from typing import Optional
 
 LOGGING_ON = True
@@ -10,30 +10,6 @@ LISTENFIRST = False
 PRINT_KEYS = False
 
 DEFAULT_STRING = "hey look ma, it's a simulation!"
-
-def filter_non_typable_chars(input_string: str) -> str:
-    """
-    Filter out non-typable characters from a string.
-    """
-    print(input_string)
-    # Define specific replacements
-    
-    replacements = {
-        '\u2028': '\n',  # replace line separator with newline
-        # '\u2029': '\n',  # replace paragraph separator with newline
-        # add more replacements here if needed
-    }
-    typable_chars = string.ascii_letters + string.digits + string.punctuation + ' \n\t'
-    for c in input_string:
-        if c not in typable_chars:
-            print(f"Non-typable character:{c}-> {ord(c)}")
-    for old, new in replacements.items():
-        input_string = input_string.replace(old, new)
-    # Filter out non-typable characters
-    filtered_string = ''.join(c for c in input_string if c in typable_chars)
-    # filtered_string = "test :("
-    return filtered_string
-
 
 def main(input_string: Optional[str] = DEFAULT_STRING, listen_first: bool = LISTENFIRST) -> None:
     """
@@ -94,7 +70,11 @@ if __name__ == "__main__":
             main(None, True)
         elif arg_string == '*Clipboard*':
             from pyperclip import paste as py_paste # type: ignore
-            main(filter_non_typable_chars(py_paste()))
+            if not py_paste():
+                print("No text found in clipboard.")
+                pass
+            else:
+                main(filter_non_typable_chars(py_paste()))
         else:
             print("Gotta have a valid argument. Try 'Test' or '*Clipboard*'")
             pass
